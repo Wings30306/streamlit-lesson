dbc337d0f1b415c62c4b7e632f55248a02f503fd
Joke Heyndels <jo_hannah@outlook.com>
2022-01-13 07:31:22 +0000

    Calculator lesson

--- app_pages/page_calculator.py
+++ app_pages/page_calculator.py
@@ -1,2 +1,25 @@
+import streamlit as st
 
-    
+def calculator_body():
+    st.write("---")
+    col1, col2, col3 = st.beta_columns(3)
+    with col1:
+        num1 = st.number_input(label = "Enter the first integer", step = 1)
+    with col2:
+        num2 = st.number_input(label = "Enter the second integer", step = 2)
+    with col3:
+        operator = st.selectbox(label = "Select the operator type",
+                                options = ["Add", "Subtract", "Multiply", "Divide"])
+    if st.button("Click here to calculate!"):
+        if num2 == 0 and operator == "Divide":
+            st.error("Cannot divide by 0. Choose a different integer")
+        else:
+            calculator_function(num1, num2, operator)
+
+
+def calculator_function(num1, num2, operator):
+    if operator == "Add": result = num1 + num2
+    elif operator == "Subtract": result = num1 - num2
+    elif operator == "Multiply": result = num1 * num2
+    elif operator == "Divide": result = num1 / num2
+    st.success(f'The result is: **{result}**')
